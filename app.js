2375f201504d67f513820923f9d8a7a1ebb90040
Hemant Nirmalkar <hemantnirmalkar17@gmail.com>
2019-09-09 19:32:54 +0530

    rearrangements for better interpretation

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const localStrategy = require('passport-local')
 const passportLocalMongoose = require('passport-local-mongoose')
 const User = require('./models/user')
 
-
 mongoose.connect('mongodb+srv://hemant123:hemant123@cluster0-wjckl.gcp.mongodb.net/test?retryWrites=true&w=majority')
+
 const app = express()
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('express-session')({
   secret: 'Downey you are the best',
@@ -16,11 +17,10 @@ app.use(require('express-session')({
   saveUninitialized: false
 }))
 
-app.set('view engine', 'ejs')
 app.use(passport.initialize())
 app.use(passport.session())
 
-passport.use(new localStrategy(User.authenticate))
+passport.use(new localStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 // *********************************************
