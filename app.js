104ad47533c1a98876162b196a63a9bf93b86541
Hemant Nirmalkar <hemantnirmalkar17@gmail.com>
2019-09-09 17:34:14 +0530

    post method for register

--- app.js
+++ app.js
@@ -21,18 +21,31 @@ app.use(passport.initialize())
 app.use(passport.session())
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
+// *********************************************
+// ROUTES
+// *********************************************
 
-
+// LEADS TO LANDING PAGE 
 app.get('/', function (req, res) {
   res.render('landing')
 })
+
+// LEADS TO SECRET PAGE
 app.get('/secret', function (req, res) {
   res.render('secret')
 })
+
+// FOR SIGN UP FORM 
 app.get('/register', function (req, res) {
   res.render('register')
 })
+//POST METHOD FOR REGISTER
+app.post('/register', function (req, res) {
+  res.send('connected')
+})
 
+
+// LEADS TO LOGIN PAGE 
 app.get('/login', function (req, res) {
   res.render('login')
 })
