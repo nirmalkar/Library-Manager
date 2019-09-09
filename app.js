443bfa22115ab32965c13c68d0b82ac4d094c0bc
Hemant Nirmalkar <hemantnirmalkar17@gmail.com>
2019-09-09 18:39:57 +0530

    passing two arguments to save username and password(hash version or encoded one) to the DB

--- app.js
+++ app.js
@@ -43,6 +43,15 @@ app.get('/register', function (req, res) {
 app.post('/register', function (req, res) {
   req.body.username
   req.body.password
+  User.register(new User({ username: req.body.username }), req.body.password, function (err, user) {
+    if (err) {
+      console.log(err)
+      return res.render('register')
+    }
+    passport.authenticate('local')(req, res, function () {
+      res.redirect('/secret')
+    })
+  })
 })
 
 
